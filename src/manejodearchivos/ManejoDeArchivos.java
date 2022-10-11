be24fbeef2d7e29a76111b94447de2cfcdc31a6a
Ana María Gómez Fandiño <84543307+anaMariaGF@users.noreply.github.com>
2022-10-11 10:08:10 +0200

    Initial commit

---
+++ src/manejodearchivos/ManejoDeArchivos.java
@@ -0,0 +1,23 @@
+/*
+ * Crear archivo
+ *Guardar información en un archivo
+ *Eliminar  información de un archivo 
+ *eliminar un archivo
+ */
+package manejodearchivos;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class ManejoDeArchivos {
+
+    /**
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
